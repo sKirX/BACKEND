8413ddfc89ef3a490ca60f4f2c55d37600d596fd
SkirX <m0946037250gmail.com>
2025-11-11 13:53:51 +0700

    first commit

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 // ใช้ค่าจาก .env
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, //เพิ่ม port
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
